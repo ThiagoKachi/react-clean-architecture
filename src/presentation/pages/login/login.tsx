b3a639a254059337ed7e8bb85d82d77026502966
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-03-25 20:34:57 -0300

    feat: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -27,7 +27,7 @@ export default function Login() {
           <Input type="email" name="email" placeholder='Digite seu e-mail' />
           <Input type="password" name="password" placeholder='Digite sua senha' />
 
-          <button type="submit" className={Styles.submit}>Entrar</button>
+          <button data-testid="submit" disabled type="submit" className={Styles.submit}>Entrar</button>
           <span className={Styles.link}>Criar conta</span>
 
           <FormStatus />
