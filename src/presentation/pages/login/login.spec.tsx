f439403000d44ba4b49e7d5a192dbd54beffb6ff
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-03-27 19:55:15 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -58,4 +58,15 @@ describe('Login', () => {
       email: 'any_email'
     })
   });
+
+  it('should call validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+
+    const passwordInput = screen.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+
+    expect(validationSpy.input).toEqual({
+      password: 'any_password',
+    })
+  });
 });
