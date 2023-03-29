fc81c4287b76d89ea3ce0cd7342cc501b8a40d76
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-03-29 20:35:30 -0300

    feat: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,51 +1,67 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import { Footer, LoginHeader, Input, FormStatus } from '@/presentation/components'
-import Context from '@/presentation/contexts/form/form-context'
-import { Validation } from '@/presentation/protocols/validation'
+import {
+  Footer,
+  LoginHeader,
+  Input,
+  FormStatus,
+} from "@/presentation/components";
+import Context from "@/presentation/contexts/form/form-context";
+import { Validation } from "@/presentation/protocols/validation";
 
-import Styles from './login-styles.scss'
+import Styles from "./login-styles.scss";
 
 type Props = {
-  validation: Validation
-}
+  validation: Validation;
+};
 
 export default function Login({ validation }: Props) {
   const [state, setState] = useState({
     isLoading: false,
-    email: '',
-    password: '',
-    emailError: '',
-    passwordError: '',
-    mainError: '',
+    email: "",
+    password: "",
+    emailError: "",
+    passwordError: "",
+    mainError: "",
   });
 
   useEffect(() => {
     setState((oldState) => ({
       ...oldState,
-      emailError: validation.validate('email', state.email),
-    }))
-  }, [state.email])
+      emailError: validation.validate("email", state.email),
+    }));
+  }, [state.email]);
 
   useEffect(() => {
     setState((oldState) => ({
       ...oldState,
-      passwordError: validation.validate('password', state.password),
-    }))
-  }, [state.password])
+      passwordError: validation.validate("password", state.password),
+    }));
+  }, [state.password]);
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
 
-      <Context.Provider value={{ state, setState }}>      
+      <Context.Provider value={{ state, setState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
 
-          <Input type="email" name="email" placeholder='Digite seu e-mail' />
-          <Input type="password" name="password" placeholder='Digite sua senha' />
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
 
-          <button data-testid="submit" disabled type="submit" className={Styles.submit}>Entrar</button>
+          <button
+            data-testid="submit"
+            disabled={!!state.emailError || !!state.passwordError}
+            type="submit"
+            className={Styles.submit}
+          >
+            Entrar
+          </button>
           <span className={Styles.link}>Criar conta</span>
 
           <FormStatus />
@@ -54,5 +70,5 @@ export default function Login({ validation }: Props) {
 
       <Footer />
     </div>
-  )
+  );
 }
